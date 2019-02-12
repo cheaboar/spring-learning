374ada5c4afdce53beb26c415a871d8934719d83
陈奇波 <826802085@qq.com>
2019-02-12 20:29:05 +0800

    spring第一课

---
+++ src/main/java/SpringHelloWorld.java
@@ -0,0 +1,6 @@
+public class SpringHelloWorld implements HelloWorld{
+    @Override
+    public void sayHello() {
+        System.out.println("Struts Say Hello!!");
+    }
+}
---
+++ src/main/java/StrutsHelloWorld.java
@@ -0,0 +1,6 @@
+public class StrutsHelloWorld implements HelloWorld {
+    @Override
+    public void sayHello() {
+        System.out.println("Struts Say Hello!!");
+    }
+}
